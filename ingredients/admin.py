0562e70dede49f073ca4e0453e06666c26632e39
HMoss <htmoss7@gmail.com>
2021-03-16 20:31:13 -0700

    added metric model
    ie lbs, oz, cups, ...

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Ingredient, FoodType
+from .models import Ingredient, FoodType, Metric
 
 admin.site.register(Ingredient)
 admin.site.register(FoodType)
+admin.site.register(Metric)
--- ingredients/models.py
+++ ingredients/models.py
@@ -9,11 +9,11 @@ class FoodType(models.Model):
         return self.type
 
 
-# class Metric(models.Model):
-#     metric = models.CharField(max_length=20, default="", unique=True)
+class Metric(models.Model):
+    metric = models.CharField(max_length=20, default="", unique=True)
 
-#     def __str__(self):
-#         return self.metric
+    def __str__(self):
+        return self.metric
 
 
 class Ingredient(models.Model):
