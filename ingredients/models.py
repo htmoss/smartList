89068f76e87a2af159e2f6827307d3d3af2688ce
HMoss <htmoss7@gmail.com>
2021-03-18 20:49:03 -0700

    have ingredients model with foodType, metric, and amount

--- ingredients/models.py
+++ ingredients/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class FoodType(models.Model):
-    type = models.CharField(max_length=200, default="",
-                            help_text='Think different sections of the grocery store, like meat, bread, or vegetables')
+    foodType = models.CharField(max_length=200, default="",
+                                help_text='Think different areas of the grocery store, like meat, bread, or vegetables')
 
     def __str__(self):
-        return self.type
+        return self.foodType
 
 
 class Metric(models.Model):
@@ -19,10 +19,14 @@ class Metric(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=200, default="",
                             unique=True, primary_key=True)
-    # ing_have = models.BooleanField(blank=False)
     amount = models.IntegerField(default=1)
-    # metric = models.ForeignKey(Metric, on_delete=models.CASCADE, default="")
-    type = models.ForeignKey(FoodType, on_delete=models.CASCADE)
+    metric = models.ForeignKey(
+        Metric, on_delete=models.CASCADE, default="", blank=True, null=True)
+    foodType = models.ForeignKey(
+        FoodType, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        if(self.metric):
+            return "{}{} {}".format(self.amount, self.metric, self.name.lower().capitalize())
+        else:
+            return "{} {}".format(self.amount, self.name.lower().capitalize())
